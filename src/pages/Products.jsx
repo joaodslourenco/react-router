d0780b7872e737ec96b0c8ab5036143d25527660
João Lourenço <joaodslourenco@gmail.com>
2022-04-19 15:45:57 -0300

    added switch and nested routes

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Products = () => {
-  return <h1>Products</h1>
+  return (
+    <section>
+      <h1>Products</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">Online Course</Link>
+        </li>
+      </ul>
+    </section>
+  )
 }
 
 export default Products
